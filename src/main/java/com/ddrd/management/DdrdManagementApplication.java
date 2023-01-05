1337ae152bea27e4cb7ddc09a221ffdfaf2fe665
dong-hyeon <dlehgus6251@gmail.com>
2023-01-05 21:14:12 +0900

    ddrd management 프로젝트 생성
    ### name
    ddrd-management
    
    ### build tools
    gradle - kotiln
    
    ### JDK
    corretoo-17
    
    ### JAVA
    17
    
    ### PACKAGING
    JAR
    
    ### SPRING BOOT
    3.0.1
    
    ### DB
    MYSQL
    
    ##Infra
    AWS
    
    ###서버 환경
    linux

---
+++ src/main/java/com/ddrd/management/DdrdManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ddrd.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdrdManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdrdManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddrd/management/DdrdManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddrd.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdrdManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
